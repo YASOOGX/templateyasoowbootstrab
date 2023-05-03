1f229256edd311878f9fb97baf7e1eadfa270e35
Yousef Mostafa <115595202+YASOOGX@users.noreply.github.com>
2023-05-03 06:08:49 +0300

    Add files via upload

---
+++ js/task_1.js
@@ -0,0 +1,11 @@
+
+// start toggle menu 
+let menu = document.getElementsByTagName("button");
+let togle = document.querySelector(".links ul ");
+menu[0].onclick = function () {
+    this.classList.toggle("test");
+    togle.classList.toggle("test2")
+}
+// end togglemenu
+
+// YASOO task_1
